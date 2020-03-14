c85d27a8d7e6c0625d373fa47e8ac1407b81dbcb
ashesh1105 <ashesh1105@gmail.com>
2020-03-14 07:23:56 -0700

    Added explanation of CountSubArraysNegativeSum.java

--- DataStructures/src/main/java/com/algorithms/hackerrank/arrays/CountSubArraysNegativeSum.java
+++ DataStructures/src/main/java/com/algorithms/hackerrank/arrays/CountSubArraysNegativeSum.java
@@ -2,6 +2,24 @@ package com.algorithms.hackerrank.arrays;
 
 import java.util.Scanner;
 
+/*
+Find all sub arrays with sum of elements which are negative. Take two line inputs from user, 1st one will be number
+of lines and second line will contain space separated integers for array elements. Print total number of sub arrays
+which have overall negative count. Note: sub arrays are contiguous elements in forward direction.
+
+Example:
+Input:
+3
+1 -2 1
+Output:
+3
+Explanation:
+Sum of [1, -2] = -1
+Sum of [-2] = -2
+Sum of [-2, 1] = -1
+Result: 3
+ */
+
 public class CountSubArraysNegativeSum {
 
     public static void main(String[] args) {
